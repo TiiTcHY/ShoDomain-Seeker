f06c4546a651734295d85124a1c01523c8691beb
TiiTcHY <43418761+TiiTcHY@users.noreply.github.com>
2023-12-20 10:53:14 +0000

    Update ShoDomain_Seeker.py
    Updated to change the following: 
    - Added colour options to the output
    - added version and creator + blog to the banner
    - Added a print statement to show what domain your searching

--- ShoDomain_Seeker.py
+++ ShoDomain_Seeker.py
@@ -2,10 +2,30 @@ import argparse
 import requests
 import json
 import sys
+from colorama import init, Fore
 
 URL = "https://api.shodan.io"
 URL_DOMAIN = "https://api.shodan.io/dns/domain/"
 
+R = '\033[31m'  # red
+G = '\033[32m'  # green
+C = '\033[36m'  # cyan
+W = '\033[0m'  # white
+Y = '\033[33m'  # yellow
+
+VERSION = '1.0.2'
+blog = 'https://tiitchy.medium.com/'
+github = 'https://github.com/TiiTcHY'
+
+ascii_art = r"""
+   _____ _           _____                        _          _____           _             
+  / ____| |         |  __ \                      (_)        / ____|         | |            
+ | (___ | |__   ___ | |  | | ___  _ __ ___   __ _ _ _ __   | (___   ___  ___| | _____ _ __ 
+  \___ \| '_ \ / _ \| |  | |/ _ \| '_ ` _ \ / _` | | '_ \   \___ \ / _ \/ _ \ |/ / _ \ '__|
+  ____) | | | | (_) | |__| | (_) | | | | | | (_| | | | | |  ____) |  __/  __/   <  __/ |   
+ |_____/|_| |_|\___/|_____/ \___/|_| |_| |_|\__,_|_|_| |_| |_____/ \___|\___|_|\_\___|_|   
+                                                                                           
+    """
 class API:
     def __init__(self, key):
         self.api_key = key
@@ -32,15 +52,14 @@ class API:
             raise Exception(f"Something went wrong: {e}")
 
 def print_ascii_art():
-    print(r"""
-   _____ _           _____                        _          _____           _             
-  / ____| |         |  __ \                      (_)        / ____|         | |            
- | (___ | |__   ___ | |  | | ___  _ __ ___   __ _ _ _ __   | (___   ___  ___| | _____ _ __ 
-  \___ \| '_ \ / _ \| |  | |/ _ \| '_ ` _ \ / _` | | '_ \   \___ \ / _ \/ _ \ |/ / _ \ '__|
-  ____) | | | | (_) | |__| | (_) | | | | | | (_| | | | | |  ____) |  __/  __/   <  __/ |   
- |_____/|_| |_|\___/|_____/ \___/|_| |_| |_|\__,_|_|_| |_| |_____/ \___|\___|_|\_\___|_|   
-                                                                                           
-    """)
+    """
+    prints the program banners
+    """
+    print(f'{R}{ascii_art}{W}\n')
+    print(f'{G}[+] {Y}Version      : {W}{VERSION}')
+    print(f'{G}[+] {Y}Created By   : {W}TiiTcHY')
+    print(f'{G} \u2514\u27A4 {Y}Blog         : {W}{blog}')
+    print(f'{G} \u2514\u27A4 {Y}Github       : {W}{github}\n')
 
 def main():
     print_ascii_art()
@@ -53,21 +72,22 @@ def main():
     args = parser.parse_args()
 
     if not args.domain or not args.shodan_key:
-        print(f"[*] Usage: {sys.argv[0]} -d target.com -s ShodanAPIKey")
+        print(f"{R}[*] {C}Usage: {sys.argv[0]} -d target.com -s ShodanAPIKey")
         sys.exit(1)
 
     api = API(args.shodan_key)
 
     try:
         info = api.info_account()
-        print(f"[*] Credits: {info['query_credits']}\nScan Credits: {info['scan_credits']}\n")
+        print(f"{R}Target Domain: {G}"+ args.domain + '\n')
+        print(f"{R}[*] {C}Credits: {G}{info['query_credits']}\n{Y}\u2514\u27A4{C}Scan Credits: {G}{info['scan_credits']}\n")
 
         domain_search = args.domain
         subdomain_data = api.get_subdomain(domain_search)
 
         for v in subdomain_data['data']:
             d = v['subdomain'] + subdomain_data['domain']
-            output = f"[*] Domain: {d}\nIP/DNS: {v['value']}\nLast Scan made by Shodan: {v['last_seen']}\n"
+            output = f"{R}[*] {C}Domain: {G}{d}\n{Y}\u2514\u27A4 {C}IP/DNS: {G}{v['value']}\n{Y}\u2514\u27A4 {C}Last Scan made by Shodan: {G}{v['last_seen']}\n{W}"
 
             print(output)
 
